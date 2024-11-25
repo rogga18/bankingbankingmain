023fa45cf63bdfb640db42c97c301d44ecade550
rogga18 <rdorbayi@gmail.com>
2024-11-25 16:01:58 -0500

    Update bank.actions.ts
    bank things changed

--- lib/actions/bank.actions.ts
+++ lib/actions/bank.actions.ts
@@ -23,7 +23,7 @@ export const getAccounts = async ({ userId }: getAccountsProps) => {
     const accounts = banks?.map((bank: Bank) => ({
       id: bank.$id,
       availableBalance: 6000,
-      currentBalance: ,
+      currentBalance:000 ,
       name: "Savings Plus",
       officialName: "Horizon Bank Account",
       mask: "6775",
@@ -35,7 +35,7 @@ export const getAccounts = async ({ userId }: getAccountsProps) => {
     }));
 
     const totalBanks = accounts?.length || 0;
-    const totalCurrentBalance = (600000 * totalBanks).toFixed(2);
+    const totalCurrentBalance = (00 * totalBanks).toFixed(2);
 
     return parseStringify({
       data: accounts,
@@ -55,7 +55,7 @@ export const getAccount = async ({ appwriteItemId }: getAccountProps) => {
     const account = {
       id: bank.$id,
       availableBalance: 6000,
-      currentBalance: ,
+      currentBalance:00,
       name: "Lillian Justice",
       officialName: "Horizon Bank Account",
       mask: "1547",
